4b6f50f171154e7e946458ed25aedcdf2ad31819
Viktor <grigirev.vital1981@gmail.com>
2024-01-04 11:46:26 +1300

    fix

--- app/Http/Resources/BaseCar/BaseCarResource.php
+++ app/Http/Resources/BaseCar/BaseCarResource.php
@@ -9,17 +9,31 @@ class BaseCarResource extends JsonResource
 {
     public function toArray(Request $request): array
     {
+        $year_from_str = str_pad($this->month_start,2,0,STR_PAD_LEFT) . '.'.
+            $this->year_start;
+        if ($this->month_stop && $this->year_stop) {
+            $year_end_str = str_pad($this->month_stop,2,0,STR_PAD_LEFT) . '.'.
+                $this->year_stop;
+        } else {
+            $year_end_str = 'now';
+        }
+
         return [
             'id' => $this->id,
-            'model_year' => $this->model_year,
-            'mileage' => $this->mileage,
-            'engine_type' => $this->engine_type ? explode('#', $this->engine_type) : [],
-            'engine_size' => $this->engine_size,
-            'power' => $this->power,
-            'fuel' => $this->fuel,
-            'transmission' => $this->transmission,
-            'drivetrain' => $this->drivetrain,
-            'color' => $this->color,
+            'make' => $this->make,
+            'model' => $this->model,
+            'generation' => $this->generation,
+            'generation_number' => $this->generation_number,
+            'body_type' => $this->body_type,
+            'doors' => $this->doors,
+            'month_start' => $this->month_start,
+            'month_stop' => $this->month_stop,
+            'year_start' => $this->year_start,
+            'year_stop' => $this->year_stop,
+            'restyle' => $this->restyle,
+            'not_restyle' => $this->not_restyle,
+            'header' => $this->header,
+            'years_string' => $year_from_str . '-' . $year_end_str,
             'nomenclature' => [
                 'make' => $this->nomenclatureBaseItem->make,
                 'model' => $this->nomenclatureBaseItem->model,
