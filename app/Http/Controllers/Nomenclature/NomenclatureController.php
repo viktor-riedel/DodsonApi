d106a7a0e5e0908e063175ce9f42543639d6dfa5
Viktor <grigirev.vital1981@gmail.com>
2024-02-02 21:53:19 +1300

    fix

--- app/Http/Controllers/Nomenclature/NomenclatureController.php
+++ app/Http/Controllers/Nomenclature/NomenclatureController.php
@@ -16,31 +16,31 @@ class NomenclatureController extends Controller
     }
     
     // api calls
-    public function getCatalogMakes()
+    public function getCatalogMakes(): \Illuminate\Http\JsonResponse
     {
         $data = $this->apiHelper->loadMakes();
         return response()->json($data);
     }
 
-    public function getCatalogModels(int $make)
+    public function getCatalogModels(int $make): \Illuminate\Http\JsonResponse
     {
         $data = $this->apiHelper->loadModels($make);
         return response()->json($data);
     }
 
-    public function getCatalogGenerations(int $model)
+    public function getCatalogGenerations(int $model): \Illuminate\Http\JsonResponse
     {
         $data = $this->apiHelper->loadGenerations($model);
         return response()->json($data);
     }
 
-    public function getCatalogMvrsHeaders(int $make, int $model)
+    public function getCatalogMvrsHeaders(int $make, int $model): \Illuminate\Http\JsonResponse
     {
         $data = $this->apiHelper->loadMvrHeaders($make, $model);
         return response()->json($data);
     }
 
-    public function getCatalogPdr(int $mvrId)
+    public function getCatalogPdr(int $mvrId): \Illuminate\Http\JsonResponse
     {
         $data = $this->apiHelper->loadPdr($mvrId);
         return response()->json($data);
