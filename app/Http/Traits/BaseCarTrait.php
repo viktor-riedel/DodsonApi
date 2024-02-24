e48d0cfe3cd28019cf64c6fc6366a4abf0771463
Viktor <grigirev.vital1981@gmail.com>
2024-02-24 21:05:51 +1300

    more changes

--- app/Http/Traits/BaseCarTrait.php
+++ app/Http/Traits/BaseCarTrait.php
@@ -3,6 +3,10 @@
 namespace App\Http\Traits;
 
 use App\Models\NomenclatureBaseItem;
+use App\Models\NomenclatureBaseItemModification;
+use App\Models\NomenclatureBaseItemPdrCard;
+use App\Models\NomenclatureBaseItemPdrPositionPhoto;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 trait BaseCarTrait
@@ -104,5 +108,49 @@ trait BaseCarTrait
             });
 
         return response()->json($modifications);
+    }
+
+    public function partsList(Request $request, string $make, string $model, string $generation)
+    {
+        $query = NomenclatureBaseItem::query();
+        $query->where(['make' => $make, 'model' => $model, 'generation' => $generation]);
+        $modification = $request->toArray();
+        $baseItemsIds = $query->get()->pluck('id')->toArray();
+
+        $data = DB::table('nomenclature_base_item_pdrs')
+            ->selectRaw('distinct nomenclature_base_item_pdr_positions.id,
+                                   nomenclature_base_item_pdrs.item_name_eng,
+                                   nomenclature_base_item_pdrs.item_name_ru,
+                                   nomenclature_base_item_pdr_positions.ic_number,
+                                   nomenclature_base_item_pdr_positions.oem_number,
+                                   nomenclature_base_item_pdr_positions.ic_description,
+                                   nomenclature_base_items.generation')
+            ->join('nomenclature_base_item_pdr_positions',
+                'nomenclature_base_item_pdr_positions.nomenclature_base_item_pdr_id',
+                '=',
+                'nomenclature_base_item_pdrs.id')
+            ->join('nomenclature_base_items', 'nomenclature_base_items.id', '=' , 'nomenclature_base_item_pdrs.nomenclature_base_item_id')
+            ->join('nomenclature_base_item_modifications',
+                    'nomenclature_base_item_modifications.nomenclature_base_item_pdr_position_id',
+                    '=',
+                    'nomenclature_base_item_pdr_positions.id')
+            ->whereIn('nomenclature_base_item_id', $baseItemsIds)
+            ->where('nomenclature_base_item_modifications.header', $modification['header'])
+            ->when(!isset($modification['restyle']), function($q) {
+                return $q->whereNull('nomenclature_base_item_modifications.restyle');
+            })
+            ->when(isset($modification['restyle']), function ($q) use ($modification) {
+                return $q->where('nomenclature_base_item_modifications.restyle', (int) $modification['restyle']);
+            })
+            ->where('nomenclature_base_item_pdr_positions.is_virtual', false)
+            ->whereNull('nomenclature_base_item_pdrs.deleted_at')
+            ->get()
+            ->each(function($item) {
+                $item->photos = NomenclatureBaseItemPdrPositionPhoto::where('nomenclature_base_item_pdr_position_id', $item->id)->get();
+                $item->modifications = NomenclatureBaseItemModification::where('nomenclature_base_item_pdr_position_id', $item->id)->get();
+                $item->card = NomenclatureBaseItemPdrCard::where('nomenclature_base_item_pdr_position_id', $item->id)->first();
+            });
+
+        return response()->json($data);
     }
 }
--- routes/Cars/CreateNewCar.php
+++ routes/Cars/CreateNewCar.php
@@ -5,9 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('create-car')->middleware('auth:sanctum')->group(function() {
    Route::get('makes', [CreateCarController::class, 'makes']);
-   Route::get('{make}/models', [CreateCarController::class, 'models']);
-   Route::get('{make}/{model}/generations', [CreateCarController::class, 'generations']);
-   Route::get('{make}/{model}/{generation}/modifications', [CreateCarController::class, 'modifications']);
+   Route::get('/{make}/models', [CreateCarController::class, 'models']);
+   Route::get('/{make}/{model}/generations', [CreateCarController::class, 'generations']);
+   Route::get('/{make}/{model}/{generation}/modifications', [CreateCarController::class, 'modifications']);
+   Route::post('/{make}/{model}/{generation}/parts-list', [CreateCarController::class, 'partsList']);
    Route::post('/upload-image', [CreateCarController::class, 'uploadPhoto']);
    Route::post('/create-new-car', [CreateCarController::class, 'createNewCar']);
 });
