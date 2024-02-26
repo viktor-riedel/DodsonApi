d41691e11f050ce3b185b38cb08fc299c0a16fd7
Viktor <grigirev.vital1981@gmail.com>
2024-02-26 22:45:14 +1300

    fix

--- app/Http/Controllers/AllCars/AllCarsController.php
+++ app/Http/Controllers/AllCars/AllCarsController.php
@@ -11,7 +11,7 @@ class AllCarsController extends Controller
     public function list()
     {
         $cars = Car::with('images', 'carAttributes', 'modification')
-            ->orderBy('created_at', 'desc')
+            ->orderBy('created_at')
             ->paginate(20);
 
         return CarResource::collection($cars);
