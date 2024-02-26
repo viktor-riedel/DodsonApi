7caa2c1f009f2ce3fc2c7e5cb98f48e090641c71
Viktor <grigirev.vital1981@gmail.com>
2024-02-26 22:43:40 +1300

    more changes

--- app/Http/Controllers/EditCar/EditCarController.php
+++ app/Http/Controllers/EditCar/EditCarController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\EditCar;
 use App\Http\Controllers\Controller;
 use App\Http\Traits\CarPdrTrait;
 use App\Models\Car;
+use App\Models\MediaFile;
+use Illuminate\Http\Request;
 
 class EditCarController extends Controller
 {
@@ -23,5 +25,43 @@ class EditCarController extends Controller
            'parts_list' => $partsList,
            'car_statuses' => Car::CAR_STATUSES,
         ]);
+    }
+
+    public function uploadCarPhoto(Request $request, Car $car): \Illuminate\Http\JsonResponse
+    {
+        if ($request->file('uploadCarPhotos')) {
+            $storage = \Storage::disk('s3');
+            foreach ($request->file('uploadCarPhotos') as $file) {
+                $fileName = \Str::random();
+                $originFileName = $file->getFilename();
+                $folderName = 'cars/new/'.\Str::random();
+                $mime = $file->getMimeType();
+                $fileExtension = '.'.$file->clientExtension();
+                $savePath = $folderName.'/'.$fileName.$fileExtension;
+                $size = $file->getSize();
+                $storage->put($savePath, $file->getContent(), 'public');
+                $car->images()->create([
+                    'url' => $storage->url($savePath),
+                    'mime' => $mime,
+                    'original_file_name' => $originFileName,
+                    'folder_name' => $folderName,
+                    'extension' => $fileExtension,
+                    'file_size' => $size,
+                    'special_flag' => null,
+                    'created_by' => $request->user()->id,
+                ]);
+            }
+        }
+        return response()->json($car->images);
+    }
+
+    public function deleteCarPhoto(Request $request, Car $car, MediaFile $photo)
+    {
+        $photo = $car->images()->where('id', $photo->id)->first();
+        if ($photo) {
+            $photo->update(['deleted_by' => $request->user()->id]);
+            $photo->delete();
+        }
+        return response()->json($car->images);
     }
 }
