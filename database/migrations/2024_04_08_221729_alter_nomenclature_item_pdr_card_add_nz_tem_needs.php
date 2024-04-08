62f1e165e9bd43ef907d41e46d413921b64a2563
Viktor <grigirev.vital1981@gmail.com>
2024-04-08 22:21:31 +1200

    added nz needs

---
+++ database/migrations/2024_04_08_221729_alter_nomenclature_item_pdr_card_add_nz_tem_needs.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('nomenclature_base_item_pdr_cards', function (Blueprint $table) {
+            $table->integer('nz_team_needs')->nullable()->after('nz_team_price');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('nomenclature_base_item_pdr_cards', function (Blueprint $table) {
+            $table->dropColumn('nz_team_needs');
+        });
+    }
+};
