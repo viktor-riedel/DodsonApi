7776dbb9e7967bea90a6668e03131deb86ce5b1f
Viktor <grigirev.vital1981@gmail.com>
2024-06-18 13:09:36 +1200

    fix

--- routes/Auth/auth.php
+++ routes/Auth/auth.php
@@ -14,3 +14,40 @@ Route::prefix('auth')->group(function() {
 
    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 });
+
+//Route::get('test', function() {
+//    $car = \App\Models\Car::find(54);
+//    $partsList = \DB::table('cars')
+//        ->selectRaw('car_pdr_position_cards.id,
+//            car_pdr_position_cards.parent_inner_id,
+//            car_pdr_position_cards.name_eng,
+//            car_pdr_position_cards.name_ru,
+//            car_pdr_position_cards.ic_number,
+//            car_pdr_position_cards.oem_number,
+//            car_pdr_position_cards.description as ic_description,
+//            car_pdr_position_card_prices.price_currency,
+//            car_pdr_position_card_prices.buying_price,
+//            car_pdr_position_card_prices.selling_price,
+//            car_pdr_positions.user_id,
+//            car_pdr_position_cards.barcode,
+//            users.name as client_name,
+//            car_pdr_position_cards.comment')
+//        ->join('car_pdrs', 'car_pdrs.car_id', '=', 'cars.id')
+//        ->join('car_pdr_positions','car_pdr_positions.car_pdr_id', '=', 'car_pdrs.id')
+//        ->join('car_pdr_position_cards', 'car_pdr_position_cards.car_pdr_position_id', '=', 'car_pdr_positions.id')
+//        ->leftJoin('users', 'users.id', '=', 'car_pdr_positions.user_id')
+//        ->join('car_pdr_position_card_prices', 'car_pdr_position_card_prices.car_pdr_position_card_id', '=', 'car_pdr_position_cards.id')
+//        ->where('cars.id', $car->id)
+//        ->whereNull('car_pdr_positions.deleted_at')
+//        ->orderBy('car_pdr_position_cards.name_eng')
+//        ->get()->each(function($position) {
+//            $card = App\Models\CarPdrPositionCard::with('images', 'createdBy', 'priceCard', 'partAttributesCard', 'comments', 'comments.createdBy')
+//                ->find($position->id);
+//            $position->images = $card->images ?? [];
+//            $position->card = $card ?? null;
+//        });
+//    return \Pdf::loadView('exports.pdf.dismantling-document', [
+//        'parts' => $partsList,
+//        'car' => $car,
+//    ])->stream();
+//});
