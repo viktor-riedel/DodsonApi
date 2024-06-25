8db1ab2c33e372bf1f844ffd4d9120d184d9c8c7
Viktor <grigirev.vital1981@gmail.com>
2024-06-26 09:48:58 +1200

    added printing form

---
+++ resources/views/exports/pdf/dismantling-badges.blade.php
@@ -0,0 +1,101 @@
+<!DOCTYPE  html>
+<html lang="en">
+    <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <style type="text/css"> * {margin:0; padding:0; text-indent:0; }
+        .s1 { color: black; font-family:DejaVu Sans, sans-serif; font-style: italic; font-weight: bold; text-decoration: none; font-size: 16pt; }
+        .s2 { color: black; font-family:DejaVu Sans, sans-serif; font-style: italic; font-weight: bold; text-decoration: none; font-size: 12pt; }
+        .s3 { color: black; font-family:DejaVu Sans, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 12pt; }
+        .s4 { color: black; font-family:DejaVu Sans, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 10pt; }
+        .s5 { color: black; font-family:DejaVu Sans, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 8pt; }
+        .s6 { color: black; font-family:DejaVu Sans, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 6pt; }
+        .s7 { color: black; font-family:DejaVu Sans, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 6pt; }
+        .s8 { color: black; font-family:DejaVu Sans, sans-serif; font-style: italic; font-weight: bold; text-decoration: none; font-size: 9pt; }
+        .s9 { color: black; font-family:DejaVu Sans, sans-serif; font-style: italic; font-weight: bold; text-decoration: none; font-size: 8pt; }
+        table, tbody {vertical-align: top; overflow: visible; }
+        .page_break { page-break-before: always; }
+        @page { size: 10cm 7cm landscape;}
+    </style>
+</head>
+<body>
+@foreach($parts as $key => $part)
+    <table style="border-collapse:collapse;width: 100%;margin:auto;" cellspacing="0">
+        <tr style="height:31pt">
+            <td style="width:157pt;border-top-style:dashed;border-top-width:1pt;border-top-color:#333333;border-left-style:dashed;border-left-width:1pt;border-left-color:#333333;border-bottom-style:solid;border-bottom-width:1pt;border-bottom-color:#333333" rowspan="2">
+                <p class="s1" style="margin-left: 10px; padding-left: 34pt;text-indent: 0pt;line-height: 16pt;text-align: left;">
+                    {{$part->barcode}}
+                </p>
+                <p style="text-indent: 0pt;text-align: left;">
+                    <span>
+                        <table border="0" cellspacing="0" cellpadding="0" style="margin-left: 10px">
+                            <tr>
+                                <td>
+                                    <img width="209" height="43" src="data:image/png;base64,'{{DNS1D::getBarcodePNG((string) $part->barcode, \App\Models\CarPdrPositionCard::BARCODE_ALGO)}}"/>
+                                </td>
+                            </tr>
+                        </table>
+                    </span></p>
+                <p style="padding-top: 3pt;text-indent: 0pt;text-align: left;"><br/></p>
+            </td>
+            <td style="width:88pt;border-top-style:dashed;border-top-width:1pt;border-top-color:#333333;border-bottom-style:solid;border-bottom-width:1pt;border-bottom-color:#333333;border-right-style:dashed;border-right-width:1pt;border-right-color:#333333">
+                <p class="s4" style="padding-left: 3pt;text-indent: 0pt;text-align: left;">
+                    Stock #: {{$car->car_mvr}}
+                </p>
+            </td>
+        </tr>
+        <tr style="height:32pt">
+            <td style="width:88pt;border-top-style:solid;border-top-width:1pt;border-top-color:#333333;border-bottom-style:solid;border-bottom-width:1pt;border-bottom-color:#333333;border-right-style:dashed;border-right-width:1pt;border-right-color:#333333">
+                <p class="s4" style="padding-left: 3pt;text-indent: 0pt;text-align: left;">
+                    Yr: <span class="s3">{{$car->carAttributes->year > 0 ? $car->carAttributes->year : ''}}</span>
+                </p>
+                <p class="s4" style="padding-top: 2pt;padding-left: 3pt;text-indent: 0pt;text-align: left;">
+                    IC: {{$part->ic_number}}
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td style="width:100%;height: 123pt;border-top-style:solid;border-top-width:1pt;border-top-color:#333333;border-left-style:dashed;border-left-width:1pt;border-left-color:#333333;border-bottom-style:dashed;border-bottom-width:1pt;border-bottom-color:#333333;border-right-style:dashed;border-right-width:1pt;border-right-color:#333333" colspan="2">
+                <p style="padding-top: 10pt;text-indent: 0pt;text-align: left;"><br/></p>
+                @if(count(explode(' ', $part->name_eng)) < 3)
+                    <p class="s4" style="margin-left: 5px;padding-left: 2pt;padding-right: 27pt;text-indent: 0pt;text-align: left;">
+                        {{$part->name_eng}}
+                    </p>
+                @else
+                    <p class="s8" style="margin-left: 5px;padding-left: 2pt;padding-right: 27pt;text-indent: 0pt;text-align: left;">
+                        {{$part->name_eng}}
+                    </p>
+                @endif
+                @if(count(explode(' ', $part->name_ru)) <= 4)
+                    <p class="s4" style="margin-left: 5px;padding-left: 2pt;padding-right: 27pt;text-indent: 0pt;text-align: left;">
+                        {{$part->name_ru}}
+                    </p>
+                @endif
+                @if(count(explode(' ', $part->name_ru)) > 4)
+                    <p class="s9" style="margin-left: 5px;padding-left: 2pt;padding-right: 27pt;text-indent: 0pt;text-align: left;">
+                        {{$part->name_ru}}
+                    </p>
+                @endif
+                <p class="s2" style="margin-left: 5px;padding-left: 2pt;text-indent: 0pt;text-align: left;">{{$car->make}}, {{$car->model}}</p>
+                <p class="s7" style="margin-left: 5px;padding-top: 1pt;padding-left: 1pt;text-indent: 0pt;text-align: left;">
+                    IC description:
+                    @if($part->ic_description)
+                        {{$part->ic_description}}
+                    @else
+                        <br>
+                    @endif
+                </p>
+                <p class="s5" style="margin-left: 5px;padding-top: 1pt;padding-left: 1pt;text-indent: 0pt;text-align: left;">
+                    Comment:
+                </p>
+                <p class="s6" style="margin-left: 5px;padding-top: 8pt;padding-left: 0;text-indent: 0pt;line-height: 9pt;text-align: left;">
+                    {{now()->format('d.m.Y H:i:s')}} / {{auth()->user()?->name ?? ''}}
+                </p>
+            </td>
+        </tr>
+    </table>
+    @if($key < ($parts->count() - 1))
+        <div class="page-break"></div>
+    @endif
+@endforeach
+</body></html>
--- resources/views/exports/pdf/dismantling-document.blade.php
+++ resources/views/exports/pdf/dismantling-document.blade.php
@@ -1,101 +1,240 @@
 <!DOCTYPE  html>
 <html lang="en">
-    <head>
+<head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <title></title>
-    <style type="text/css"> * {margin:0; padding:0; text-indent:0; }
-        .s1 { color: black; font-family:DejaVu Sans, sans-serif; font-style: italic; font-weight: bold; text-decoration: none; font-size: 16pt; }
-        .s2 { color: black; font-family:DejaVu Sans, sans-serif; font-style: italic; font-weight: bold; text-decoration: none; font-size: 12pt; }
-        .s3 { color: black; font-family:DejaVu Sans, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 12pt; }
-        .s4 { color: black; font-family:DejaVu Sans, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 10pt; }
-        .s5 { color: black; font-family:DejaVu Sans, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 8pt; }
-        .s6 { color: black; font-family:DejaVu Sans, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 6pt; }
-        .s7 { color: black; font-family:DejaVu Sans, sans-serif; font-style: normal; font-weight: normal; text-decoration: none; font-size: 6pt; }
-        .s8 { color: black; font-family:DejaVu Sans, sans-serif; font-style: italic; font-weight: bold; text-decoration: none; font-size: 9pt; }
-        .s9 { color: black; font-family:DejaVu Sans, sans-serif; font-style: italic; font-weight: bold; text-decoration: none; font-size: 8pt; }
-        table, tbody {vertical-align: top; overflow: visible; }
+    <meta name="author" content=""/>
+    <style type="text/css"> * {
+            margin: 0;
+            padding: 0;
+            text-indent: 0;
+        }
+
+        .s1 {
+            color: black;
+            font-family: DejaVu Sans, sans-serif;
+            font-style: normal;
+            font-weight: bold;
+            text-decoration: none;
+            font-size: 8.5pt;
+        }
+
+        p {
+            color: black;
+            font-family: DejaVu Sans, sans-serif;
+            font-style: normal;
+            font-weight: bold;
+            text-decoration: none;
+            font-size: 8.5pt;
+            margin: 0pt;
+        }
+
+        .s2 {
+            color: black;
+            font-family: DejaVu Sans, sans-serif;
+            font-style: normal;
+            font-weight: normal;
+            text-decoration: underline;
+            font-size: 8.5pt;
+        }
+
+        .s3 {
+            color: black;
+            font-family: DejaVu Sans, sans-serif;
+            font-style: normal;
+            font-weight: normal;
+            text-decoration: none;
+            font-size: 8.5pt;
+        }
+
+        .s4 {
+            color: black;
+            font-family: DejaVu Sans, sans-serif;
+            font-style: normal;
+            font-weight: normal;
+            text-decoration: none;
+            font-size: 8.5pt;
+        }
+
+        table, tbody {
+            vertical-align: top;
+            overflow: visible;
+        }
+
+        @page {
+            size: portrait;
+        }
         .page_break { page-break-before: always; }
-        @page { size: 10cm 7cm landscape;}
     </style>
 </head>
 <body>
-@foreach($parts as $key => $part)
-    <table style="border-collapse:collapse;width: 100%;margin:auto;" cellspacing="0">
-        <tr style="height:31pt">
-            <td style="width:157pt;border-top-style:dashed;border-top-width:1pt;border-top-color:#333333;border-left-style:dashed;border-left-width:1pt;border-left-color:#333333;border-bottom-style:solid;border-bottom-width:1pt;border-bottom-color:#333333" rowspan="2">
-                <p class="s1" style="margin-left: 10px; padding-left: 34pt;text-indent: 0pt;line-height: 16pt;text-align: left;">
-                    {{$part->barcode}}
-                </p>
-                <p style="text-indent: 0pt;text-align: left;">
-                    <span>
-                        <table border="0" cellspacing="0" cellpadding="0" style="margin-left: 10px">
-                            <tr>
-                                <td>
-                                    <img width="209" height="43" src="data:image/png;base64,'{{DNS1D::getBarcodePNG((string) $part->barcode, \App\Models\CarPdrPositionCard::BARCODE_ALGO)}}"/>
-                                </td>
-                            </tr>
-                        </table>
-                    </span></p>
-                <p style="padding-top: 3pt;text-indent: 0pt;text-align: left;"><br/></p>
-            </td>
-            <td style="width:88pt;border-top-style:dashed;border-top-width:1pt;border-top-color:#333333;border-bottom-style:solid;border-bottom-width:1pt;border-bottom-color:#333333;border-right-style:dashed;border-right-width:1pt;border-right-color:#333333">
-                <p class="s4" style="padding-left: 3pt;text-indent: 0pt;text-align: left;">
-                    Stock #: {{$car->car_mvr}}
-                </p>
-            </td>
-        </tr>
-        <tr style="height:32pt">
-            <td style="width:88pt;border-top-style:solid;border-top-width:1pt;border-top-color:#333333;border-bottom-style:solid;border-bottom-width:1pt;border-bottom-color:#333333;border-right-style:dashed;border-right-width:1pt;border-right-color:#333333">
-                <p class="s4" style="padding-left: 3pt;text-indent: 0pt;text-align: left;">
-                    Yr: <span class="s3">{{$car->carAttributes->year > 0 ? $car->carAttributes->year : ''}}</span>
-                </p>
-                <p class="s4" style="padding-top: 2pt;padding-left: 3pt;text-indent: 0pt;text-align: left;">
-                    IC: {{$part->ic_number}}
-                </p>
-            </td>
-        </tr>
-        <tr>
-            <td style="width:100%;height: 123pt;border-top-style:solid;border-top-width:1pt;border-top-color:#333333;border-left-style:dashed;border-left-width:1pt;border-left-color:#333333;border-bottom-style:dashed;border-bottom-width:1pt;border-bottom-color:#333333;border-right-style:dashed;border-right-width:1pt;border-right-color:#333333" colspan="2">
-                <p style="padding-top: 10pt;text-indent: 0pt;text-align: left;"><br/></p>
-                @if(count(explode(' ', $part->name_eng)) < 3)
-                    <p class="s4" style="margin-left: 5px;padding-left: 2pt;padding-right: 27pt;text-indent: 0pt;text-align: left;">
-                        {{$part->name_eng}}
+<p style="text-indent: 0pt;text-align: left;"><br/></p>
+<table style="border-collapse:collapse;width: 100%; margin: 10pt;">
+    <tr style="height:10pt">
+        <td style="width:77pt">
+            <p class="s1" style="padding-left: 2pt;text-indent: 0pt;line-height: 9pt;text-align: left;">
+                {{$car->make}} {{$car->model}}
+            </p>
+        </td>
+    </tr>
+    <tr style="height:12pt">
+        <td style="width:100pt">
+            <p class="s1" style="padding-left: 2pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                Chassis: {{$car->chassis}}
+                &nbsp;&nbsp;&nbsp;Год выпуска: {{$car->carAttributes->year ?: ''}}
+            </p>
+        </td>
+    </tr>
+    <tr style="height:10pt">
+        <td style="width:77pt">
+            <p class="s1" style="padding-left: 2pt;text-indent: 0pt;line-height: 9pt;text-align: left;">
+                MVR: {{$car->car_mvr}}
+            </p>
+        </td>
+    </tr>
+</table>
+<p style="text-indent: 0pt;text-align: left;"/>
+<p style="text-indent: 0pt;text-align: left;"><br/></p>
+<p style="padding-left: 10pt;text-indent: 0pt;text-align: left;">
+    Разборщик:
+    <span class="s3">{{auth()->user()?->name}}</span>
+    Дата:
+    <span class="s3">{{now()->format('d/m/Y')}}</span>
+    Приемщик :<span class="s2"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
+    <span class="s3"> </span>
+    Дата<span class="s2"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p>
+<p style="text-indent: 0pt;text-align: left;"><br/></p>
+<table style="border-collapse: collapse;width: 97%; margin-left: 10pt;">
+    <tr style="height:11pt">
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+            bgcolor="#7DCFE7">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                #
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+            bgcolor="#7DCFE7">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                IC number
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+            bgcolor="#7DCFE7">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                IC description
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+            bgcolor="#7DCFE7">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                Name ENG
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+            bgcolor="#7DCFE7">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                Name RU
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+            bgcolor="#7DCFE7">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                Client
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+            bgcolor="#7DCFE7">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                Comment
+            </p>
+        </td>
+    </tr>
+    @foreach($parts as $index => $part)
+        @if($index === 0)
+            <tr style="height:11pt">
+                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                    bgcolor="#7DCFE7">
+                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
+                </td>
+                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                    colspan="4" bgcolor="#7DCFE7">
+                    <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: center;">
+                        {{$part->folder}}
                     </p>
-                @else
-                    <p class="s8" style="margin-left: 5px;padding-left: 2pt;padding-right: 27pt;text-indent: 0pt;text-align: left;">
-                        {{$part->name_eng}}
+                </td>
+                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                    bgcolor="#7DCFE7">
+                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
+                </td>
+                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                    bgcolor="#7DCFE7">
+                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
+                </td>
+            </tr>
+        @elseif ($part->folder !== $parts[$index - 1]->folder)
+            <tr style="height:11pt">
+                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                    bgcolor="#7DCFE7">
+                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
+                </td>
+                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                    colspan="4" bgcolor="#7DCFE7">
+                    <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: center;">
+                        {{$part->folder}}
                     </p>
+                </td>
+                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                    bgcolor="#7DCFE7">
+                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
+                </td>
+                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                    bgcolor="#7DCFE7">
+                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
+                </td>
+            </tr>
+        @endif
+    <tr style="height:11pt">
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+            <p class="s4" style="text-indent: 0pt;line-height: 10pt;text-align: right;">
+                {{$index + 1}}
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+            <p class="s4" style="text-indent: 0pt;line-height: 10pt;text-align: right;">
+                {{$part->ic_number}}
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+            <p class="s4" style="text-indent: 0pt;line-height: 10pt;text-align: center;">
+                {{$part->ic_description}}
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                {{$part->name_eng}}
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                {{$part->name_ru}}
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                {{$part->client_name}}
+            </p>
+        </td>
+        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
+            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: center;">
+                @if($part->card->comments->count())
+                    @foreach($part->card->comments as $comment)
+                       {{$comment->comment}}<br>
+                    @endforeach
                 @endif
-                @if(count(explode(' ', $part->name_ru)) <= 4)
-                    <p class="s4" style="margin-left: 5px;padding-left: 2pt;padding-right: 27pt;text-indent: 0pt;text-align: left;">
-                        {{$part->name_ru}}
-                    </p>
-                @endif
-                @if(count(explode(' ', $part->name_ru)) > 4)
-                    <p class="s9" style="margin-left: 5px;padding-left: 2pt;padding-right: 27pt;text-indent: 0pt;text-align: left;">
-                        {{$part->name_ru}}
-                    </p>
-                @endif
-                <p class="s2" style="margin-left: 5px;padding-left: 2pt;text-indent: 0pt;text-align: left;">{{$car->make}}, {{$car->model}}</p>
-                <p class="s7" style="margin-left: 5px;padding-top: 1pt;padding-left: 1pt;text-indent: 0pt;text-align: left;">
-                    IC description:
-                    @if($part->ic_description)
-                        {{$part->ic_description}}
-                    @else
-                        <br>
-                    @endif
-                </p>
-                <p class="s5" style="margin-left: 5px;padding-top: 1pt;padding-left: 1pt;text-indent: 0pt;text-align: left;">
-                    Comment:
-                </p>
-                <p class="s6" style="margin-left: 5px;padding-top: 8pt;padding-left: 0;text-indent: 0pt;line-height: 9pt;text-align: left;">
-                    {{now()->format('d.m.Y H:i:s')}} / {{auth()->user()?->name ?? ''}}
-                </p>
-            </td>
-        </tr>
-    </table>
-    @if($key < ($parts->count() - 1))
-        <div class="page-break"></div>
-    @endif
-@endforeach
-</body></html>
+            </p>
+        </td>
+    </tr>
+    @endforeach
+</table>
+</body>
+</html>
