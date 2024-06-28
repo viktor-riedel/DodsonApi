e447eb13f6a3bd72e38a35ac330447cf7596ef8c
Viktor <grigirev.vital1981@gmail.com>
2024-06-29 11:29:57 +1200

    dismantling document

--- resources/views/exports/pdf/dismantling-document.blade.php
+++ resources/views/exports/pdf/dismantling-document.blade.php
@@ -64,177 +64,76 @@
         @page {
             size: landscape;
         }
-        .page_break { page-break-before: always; }
+
+        .page-break { page-break-after: always; }
     </style>
 </head>
 <body>
-<p style="text-indent: 0pt;text-align: left;"><br/></p>
-<table style="border-collapse:collapse;width: 100%; margin: 10pt;">
-    <tr style="height:10pt">
-        <td style="width:77pt">
-            <p class="s1" style="padding-left: 2pt;text-indent: 0pt;line-height: 9pt;text-align: left;">
-                {{$car->make}} {{$car->model}}
-            </p>
-        </td>
-    </tr>
-    <tr style="height:12pt">
-        <td style="width:100pt">
-            <p class="s1" style="padding-left: 2pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                Chassis: {{$car->chassis}}
-                &nbsp;&nbsp;&nbsp;Год выпуска: {{$car->carAttributes->year ?: ''}}
-            </p>
-        </td>
-    </tr>
-    <tr style="height:10pt">
-        <td style="width:77pt">
-            <p class="s1" style="padding-left: 2pt;text-indent: 0pt;line-height: 9pt;text-align: left;">
-                MVR: {{$car->car_mvr}}
-            </p>
-        </td>
-    </tr>
-</table>
-<p style="text-indent: 0pt;text-align: left;"/>
-<p style="text-indent: 0pt;text-align: left;"><br/></p>
-<p style="padding-left: 10pt;text-indent: 0pt;text-align: left;">
-    Разборщик:
-    <span class="s3">{{auth()->user()?->name}}</span>
-    Дата:
-    <span class="s3">{{now()->format('d/m/Y')}}</span>
-    Приемщик :<span class="s2"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span>
-    <span class="s3"> </span>
-    Дата<span class="s2"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></p>
-<p style="text-indent: 0pt;text-align: left;"><br/></p>
-<table style="border-collapse: collapse;width: 97%; margin-left: 10pt;">
-    <tr style="height:11pt">
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-            bgcolor="#7DCFE7">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                #
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-            bgcolor="#7DCFE7">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                IC number
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-            bgcolor="#7DCFE7">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                IC description
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-            bgcolor="#7DCFE7">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                Name ENG
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-            bgcolor="#7DCFE7">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                Name RU
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-            bgcolor="#7DCFE7">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                Client
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-            bgcolor="#7DCFE7">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                Comment
-            </p>
-        </td>
-    </tr>
-    @foreach($parts as $index => $part)
-        @if($index === 0)
-            <tr style="height:11pt">
-                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-                    bgcolor="#7DCFE7">
-                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
-                </td>
-                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-                    colspan="4" bgcolor="#7DCFE7">
-                    <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: center;">
-                        {{$part->folder}}
-                    </p>
-                </td>
-                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-                    bgcolor="#7DCFE7">
-                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
-                </td>
-                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-                    bgcolor="#7DCFE7">
-                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
-                </td>
-            </tr>
-        @elseif ($part->folder !== $parts[$index - 1]->folder)
-            <tr style="height:11pt">
-                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-                    bgcolor="#7DCFE7">
-                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
-                </td>
-                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-                    colspan="4" bgcolor="#7DCFE7">
-                    <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: center;">
-                        {{$part->folder}}
-                    </p>
-                </td>
-                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-                    bgcolor="#7DCFE7">
-                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
-                </td>
-                <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
-                    bgcolor="#7DCFE7">
-                    <p style="text-indent: 0pt;text-align: left;"><br/></p>
-                </td>
-            </tr>
-        @endif
-    <tr style="height:11pt">
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
-            <p class="s4" style="text-indent: 0pt;line-height: 10pt;text-align: right;">
-                {{$index + 1}}
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
-            <p class="s4" style="text-indent: 0pt;line-height: 10pt;text-align: right;">
-                {{$part->ic_number}}
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
-            <p class="s4" style="text-indent: 0pt;line-height: 10pt;text-align: center;">
-                {{$part->ic_description}}
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                {{$part->name_eng}}
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                {{$part->name_ru}}
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
-                {{$part->client_name}}
-            </p>
-        </td>
-        <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt">
-            <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: center;">
-                @if($part->card->comments->count())
-                    @foreach($part->card->comments as $comment)
-                       {{$comment->comment}}<br>
-                    @endforeach
-                @endif
-            </p>
-        </td>
-    </tr>
+    @include('exports.pdf.components.dismantling-document-header', ['car' => $car])
+    @foreach($parts->chunk(30) as $page => $chunk)
+    @if($page > 0)
+        @include('exports.pdf.components.dismantling-document-header', ['car' => $car])
+    @endif
+    <table style="border-collapse: collapse;width: 97.5%; margin-left: 10pt"
+           class="{{ !$loop->last ? 'page-break' : ''}}">
+        <tr style="height:11pt">
+            <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                bgcolor="#7DCFE7">
+                <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                    #
+                </p>
+            </td>
+            <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                bgcolor="#7DCFE7">
+                <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                    IC number
+                </p>
+            </td>
+            <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                bgcolor="#7DCFE7">
+                <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                    IC description
+                </p>
+            </td>
+            <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                bgcolor="#7DCFE7">
+                <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                    Name ENG
+                </p>
+            </td>
+            <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                bgcolor="#7DCFE7">
+                <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                    Name RU
+                </p>
+            </td>
+            <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                bgcolor="#7DCFE7">
+                <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                    Client
+                </p>
+            </td>
+            <td style="border-top-style:solid;border-top-width:1pt;border-left-style:solid;border-left-width:1pt;border-bottom-style:solid;border-bottom-width:1pt;border-right-style:solid;border-right-width:1pt"
+                bgcolor="#7DCFE7">
+                <p class="s4" style="padding-left: 1pt;text-indent: 0pt;line-height: 10pt;text-align: left;">
+                    Comment
+                </p>
+            </td>
+        </tr>
+        @foreach($chunk as $index => $part)
+            @include('exports.pdf.components.dismantling-document-table', [
+                'part' => $part,
+                'index' => $index,
+            ])
+        @endforeach
+        <tr>
+            <td colspan="7" style="text-align: left">
+                <span style="margin-top: 10px; font-size: 9px; font-family: DejaVu Sans, sans-serif;">
+                    стр {{++$page}} из {{(ceil($parts->count() / 30))}}
+                </span>
+            </td>
+        </tr>
+    </table>
     @endforeach
-</table>
 </body>
 </html>
