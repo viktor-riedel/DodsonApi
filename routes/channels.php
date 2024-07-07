e230aeab3756aebf453b1ce35e5d9e701ef4139b
Viktor <grigirev.vital1981@gmail.com>
2024-07-07 17:42:13 +1200

    added presence

--- routes/channels.php
+++ routes/channels.php
@@ -5,3 +5,16 @@ use Illuminate\Support\Facades\Broadcast;
 Broadcast::channel('user-channel.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
+
+Broadcast::channel('online-channel', function ($user) {
+    if ( $user ) {
+        return [
+            'id' => $user->id,
+            'name' => $user->name,
+            'roles' => $user->getRoleNames()->first(),
+            'country_name' => findCountryByCode($user->country_code ?? ''),
+            'email' => $user->email,
+            'connected_time' => now()->format('d.m.Y H:m'),
+        ];
+    }
+});
