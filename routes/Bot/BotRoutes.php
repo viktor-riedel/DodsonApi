46ab40b34f464821b958e73d9f826df0c14cd7e7
Viktor <grigirev.vital1981@gmail.com>
2024-07-20 10:16:00 +1200

    removed test routes

--- routes/Bot/BotRoutes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use App\Http\Controllers\Bot\BotController;
-use Illuminate\Support\Facades\Route;
-
-Route::prefix('bot')->group(function () {
-    Route::get('/stock/{car}', [BotController::class, 'car']);
-    Route::get('/stock', [BotController::class, 'stock']);
-});
