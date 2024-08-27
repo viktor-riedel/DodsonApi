c907a8748f6340197a22f0fb710944f732147312
Viktor <grigirev.vital1981@gmail.com>
2024-08-27 22:14:22 +1200

    orders search

--- routes/CRM/Orders/OrdersRoutes.php
+++ routes/CRM/Orders/OrdersRoutes.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('/orders')->group(function () {
     Route::get('/', [OrdersController::class, 'list']);
+    Route::get('/order-users', [OrdersController::class, 'users']);
     Route::get('/order-statuses', [OrdersController::class, 'statuses']);
     Route::get('/{order}', [OrdersController::class, 'view']);
     Route::patch('/{order}', [OrdersController::class, 'update']);
