b61a1601cf96e499cf09d20f27be6663a298fb83
Viktor <grigirev.vital1981@gmail.com>
2024-09-07 15:34:43 +1200

    show user balance

--- app/Http/Controllers/Balances/UserBalancesController.php
+++ app/Http/Controllers/Balances/UserBalancesController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Balances;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CRM\Balances\BalancedUserBalanceResource;
+use App\Http\Resources\CRM\Balances\BalancedUserResource;
 use App\Http\Resources\CRM\Balances\UserBalanceResource;
 use App\Http\Resources\CRM\Balances\UserResource;
 use App\Models\User;
 use App\Models\UserBalance;
+use DB;
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
@@ -40,5 +44,37 @@ class UserBalancesController extends Controller
             ->get();
 
         return UserResource::collection($users);
+    }
+
+    public function getUserBalance(Request $request, User $user): AnonymousResourceCollection
+    {
+        $balance = DB::table('user_balances')
+            ->selectRaw('
+                    user_balances.entity_name,
+                    user_balance_items.document_name,
+                    user_balance_item_documents.document_description,
+                    user_balance_item_documents.amount,
+                    case 
+		                when user_balance_item_documents.amount is null 
+		                then user_balance_items.closing_balance
+		                else null 
+		            end as closing_balance')
+            ->join('user_balance_items', function(JoinClause $join) {
+                $join->on('user_balance_items.user_balance_id', '=', 'user_balances.id');
+            })
+            ->leftJoin('user_balance_item_documents', function(JoinClause $join) {
+                $join->on('user_balance_item_documents.user_balance_item_id', '=', 'user_balance_items.id');
+            })
+            ->where('user_balances.user_id', $user->id)
+            ->whereNull('user_balances.deleted_at')
+            ->paginate(30);
+
+        return BalancedUserBalanceResource::collection($balance);
+    }
+
+    public function getBalancedUser(User $user): BalancedUserResource
+    {
+        $user->load('balance');
+        return new BalancedUserResource($user);
     }
 }
--- routes/CRM/Balances/BalancesRoutes.php
+++ routes/CRM/Balances/BalancesRoutes.php
@@ -5,4 +5,6 @@ use App\Http\Controllers\Balances\UserBalancesController;
 Route::prefix('/user-balances')->group(function () {
     Route::get('/', [UserBalancesController::class, 'list']);
     Route::get('/users', [UserBalancesController::class, 'listUsers']);
+    Route::get('/balance/{user}', [UserBalancesController::class, 'getUserBalance']);
+    Route::get('/user/{user}', [UserBalancesController::class, 'getBalancedUser']);
 });
