542c37de4e961068b92dfb36be0f9f9df0bcf376
Viktor <grigirev.vital1981@gmail.com>
2024-09-20 21:47:15 +1200

    fixed balance

--- app/Http/Controllers/Import/CarController.php
+++ app/Http/Controllers/Import/CarController.php
@@ -6,11 +6,12 @@ use App\Actions\Import\ImportFromCapartsAction;
 use App\Http\Controllers\Contracts\Importable;
 use App\Http\Controllers\Controller;
 use App\Http\ExternalApiHelpers\CapartsApiHelper;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class CarController extends Controller implements Importable
 {
-    public function importResources(): \Illuminate\Http\JsonResponse
+    public function importResources(): JsonResponse
     {
         $helper = new CapartsApiHelper();
         $cars = $helper->importUnsoldCars();
@@ -18,7 +19,7 @@ class CarController extends Controller implements Importable
         return response()->json($cars);
     }
 
-    public function importEntity(Request $request): \Illuminate\Http\JsonResponse
+    public function importEntity(Request $request): JsonResponse
     {
         $result = app()->make(ImportFromCapartsAction::class)->handle($request, $request->user()->id);
         return response()->json(['id' => $result]);
