196444858b9d58107fe174d690c86d39bad9d593
Viktor <grigirev.vital1981@gmail.com>
2024-10-08 18:16:13 +1300

    sync preorder

--- app/Actions/Order/SyncOrderAction.php
+++ app/Actions/Order/SyncOrderAction.php
@@ -3,9 +3,6 @@
 namespace App\Actions\Order;
 
 use App\Http\ExternalApiHelpers\SyncOrdersHelper;
-use App\Http\Resources\Car\CarResource;
-use App\Http\Resources\CRM\Orders\OrderResource;
-use App\Http\Resources\Order\OrderItemResource;
 use App\Models\CarPdrPosition;
 use App\Models\Order;
 
@@ -22,56 +19,113 @@ class SyncOrderAction
             $car = $order->items->first()->car;
             $car->load('images', 'carAttributes', 'modifications');
             $data = [
-                'client' => [
-                    'name' => $order->createdBy->name,
-                    'registered_name' => $order->createdBy->userCard->trading_name,
+                'base_car' => [
+                    'inner_id' => $car->modifications?->inner_id,
+                    'make' => $car->make,
+                    'model' => $car->model,
+                    'base_modification' => [
+                         "header" => $car->modifications?->header,
+                         "generation" => $car->modifications?->generation,
+                         "engine_name" => $car->modifications?->engine_name,
+                         "engine_type" => $car->modifications?->body_type,
+                         "engine_size" => $car->modifications?->engine_size,
+                         "engine_power" => $car->modifications?->engine_power,
+                         "doors" => $car->modifications?->doors,
+                         "transmission" => $car->modifications?->transmission,
+                         "drive_train" => $car->modifications?->drive_train,
+                         "chassis" => $car->modifications?->chassis,
+                         "body_type" => $car->modifications?->body_type,
+                         "restyle" => $car->modifications?->restyle,
+                         "month_from" => $car->modifications?->month_from,
+                         "month_to" => $car->modifications?->month_to,
+                         "year_from" => $car->modifications?->year_from,
+                         "year_to" => $car->modifications?->year_to,
+                    ],
                 ],
-                'id' => $car->id,
-                'make' => $car->make,
-                'model' => $car->model,
-                'year' => $car->carAttributes->year,
-                'chassis' => $car->chassis,
-                'vin' => null,
-                'modification' => $car->modifications?->body_type,
-                'header' => $car->modifications?->header,
-                'shape' => $car->modifications?->modification,
-                'generation_shape' => null,
-                'generation_number' => $car->modifications?->generation,
-                'restyle' => $car->modifications?->restyle,
-                'variant' => null,
-                'drive_train' => $car->modifications?->drive_trail,
-                'color' => null,
-                'mileage' => $car->carAttributes->mileage,
-                'year_from' => $car->modifications?->year_from,
-                'year_to' => $car->modifications?->year_to,
-                'engine_size' => $car->modifications?->engine_size,
-                'engine_name' => $car->modifications?->engine_name,
-                'engine_type' => $car->modifications?->engine_type,
-                'fuel' => null,
-                'engine_oem_number' => null,
-                'engine_ic_number' => null,
-                'engine_power' => $car->modifications?->engine_power,
-                'transmission_type' => $car->modifications?->transmission,
-                'transmission_name' => null,
-                'transmission_ic_number' => null,
-                'transmission_oem_number' => null,
-                'doors' => $car->modifications?->doors,
-                'auction_source' => $car->contr_agent_name,
-                'price' => $car->carFinance->purchase_price,
-                'stock_number' => $car->car_mvr,
-                'catalog_mvr_id' => null,
-                'generation_id' => null,
-                'modification_id' => $car->modifications?->inner_id,
+                'car' => [
+                    'make' => $car->make,
+                    'model' => $car->model,
+                    'year' => $car->carAttributes->year,
+                    'chassis' => $car->chassis,
+                    'color' => null,
+                    'mileage' => $car->carAttributes->mileage,
+                    'mvr' => [
+                        'mvr' => $car->car_mvr,
+                    ]
+                ],
+                'finance' => [
+                    'total' => $order->items->sum('price_jpy'),
+                ],
                 'images' => $car->images->map(function ($image) {
                     return $image->url;
                 })->flatten()->toArray(),
-                'total' => $order->items->sum('price_jpy'),
+                'client' => [
+                    'name' => $order->createdBy->name,
+                    'registered_name' => $order->createdBy->userCard->trading_name,
+                    'email' => $order->createdBy->email,
+                ],
             ];
-
             $syncData = $syncHelper->sendPreOrderData($data);
-            
+
             if (isset($syncData['ResultData']) && $syncData['Res']) {
                 // update order sync data
+            }
+        } else {
+            $order->load('items', 'createdBy');
+            foreach($order->items as $item) {
+                $car = CarPdrPosition::find($item->part_id)->carPdr->car;
+                $car->load('images', 'carAttributes', 'modifications');
+                $data = [
+                    'base_car' => [
+                        'inner_id' => $car->modifications?->inner_id,
+                        'make' => $car->make,
+                        'model' => $car->model,
+                        'base_modification' => [
+                            "header" => $car->modifications?->header,
+                            "generation" => $car->modifications?->generation,
+                            "engine_name" => $car->modifications?->engine_name,
+                            "engine_type" => $car->modifications?->body_type,
+                            "engine_size" => $car->modifications?->engine_size,
+                            "engine_power" => $car->modifications?->engine_power,
+                            "doors" => $car->modifications?->doors,
+                            "transmission" => $car->modifications?->transmission,
+                            "drive_train" => $car->modifications?->drive_train,
+                            "chassis" => $car->modifications?->chassis,
+                            "body_type" => $car->modifications?->body_type,
+                            "restyle" => $car->modifications?->restyle,
+                            "month_from" => $car->modifications?->month_from,
+                            "month_to" => $car->modifications?->month_to,
+                            "year_from" => $car->modifications?->year_from,
+                            "year_to" => $car->modifications?->year_to,
+                        ],
+                    ],
+                    'car' => [
+                        'make' => $car->make,
+                        'model' => $car->model,
+                        'year' => $car->carAttributes->year,
+                        'chassis' => $car->chassis,
+                        'color' => null,
+                        'mileage' => $car->carAttributes->mileage,
+                        'mvr' => [
+                            'mvr' => $car->car_mvr,
+                        ]
+                    ],
+                    'finance' => [
+                        'total' => $order->items->sum('price_jpy'),
+                    ],
+                    'images' => $car->images->map(function ($image) {
+                        return $image->url;
+                    })->flatten()->toArray(),
+                    'client' => [
+                        'name' => $order->createdBy->name,
+                        'registered_name' => $order->createdBy->userCard->trading_name,
+                        'email' => $order->createdBy->email,
+                    ],
+                ];
+                $syncData = $syncHelper->sendPreOrderData($data);
+                if (isset($syncData['ResultData']) && $syncData['Res']) {
+                    // update order sync data
+                }
             }
         }
 
